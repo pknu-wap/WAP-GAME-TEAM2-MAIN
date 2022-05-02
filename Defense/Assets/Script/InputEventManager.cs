ce0953efea22e579ad7cc862991821ea61db7a52
seungmin_cho <seungmin.cho@bagelcode.com>
2022-05-02 11:05:26 +0900

    add touch manager

---
+++ Defense/Assets/Script/InputEventManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class InputEventManager : MonoSingleton<InputEventManager>
+{
+    public delegate void TouchEvent(Touch touchInfo);
+    private event TouchEvent touchEvent;
+
+    public bool IsTouchEnable
+    {
+        get;set;
+    }
+
+    public bool AddTouchEvent(TouchEvent touchFunc)
+    {
+        foreach(var delegateFunc in touchEvent.GetInvocationList())
+            if (delegateFunc.Equals(touchFunc)) return false;
+
+        touchEvent += touchFunc;
+        return true;
+    }
+
+    public void RemoveTouchEvent(TouchEvent touchFunc)
+    {
+        touchEvent -= touchFunc;
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+            touchEvent(Input.GetTouch(0));
+    }
+}
+
