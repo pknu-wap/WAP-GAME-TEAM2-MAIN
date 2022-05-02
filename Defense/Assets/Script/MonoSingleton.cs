cfcb362d2164351292a5be82ef345f6c6d6eaa3f
seungmin_cho <seungmin.cho@bagelcode.com>
2022-05-02 11:45:32 +0900

    add logic for when no monosinglton obj found

--- Defense/Assets/Script/MonoSingleton.cs
+++ Defense/Assets/Script/MonoSingleton.cs
@@ -11,8 +11,16 @@ public class MonoSingleton<T> : MonoBehaviour where T:  MonoBehaviour
             if(instance == null)
             {
                 var objArray = Resources.FindObjectsOfTypeAll(typeof(T));
-                Assert.IsTrue(objArray.Length >= 1, string.Format("{0} shoud be only one, but you are trying to make it more than one", typeof(T).ToString()));
-                instance = objArray[0] as T;
+                if (objArray.Length > 0)
+                {
+                    Assert.IsTrue(objArray.Length >= 1, string.Format("{0} shoud be only one, but you are trying to make it more than one", typeof(T).ToString()));
+                    instance = objArray[0] as T;
+                }
+                else
+                {
+                    var newGameObj = new GameObject(typeof(T).ToString());
+                    instance = newGameObj.AddComponent<T>();
+                }
             }
 
             return instance;
