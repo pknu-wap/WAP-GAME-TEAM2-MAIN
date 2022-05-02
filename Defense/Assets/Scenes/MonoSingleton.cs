4bfbdadbf57b25998434f28712532e2d6f629276
seungmin_cho <seungmin.cho@bagelcode.com>
2022-05-02 10:44:45 +0900

    add singleton for mono

---
+++ Defense/Assets/Scenes/MonoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MonoSingleton<T> : MonoBehaviour where T:  MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                var objArray = Resources.FindObjectsOfTypeAll(typeof(T));
+                Assert.IsTrue(objArray.Length >= 1, string.Format("{0} shoud be only one, but you are trying to make it more than one", typeof(T).ToString()));
+                instance = objArray[0] as T;
+            }
+
+            return instance;
+        }
+    }
+}
