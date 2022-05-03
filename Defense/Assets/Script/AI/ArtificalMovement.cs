9362c277297e927eaee7acf65a885f75d9a4a0ef
seungmin_cho <seungmin.cho@bagelcode.com>
2022-05-03 11:43:19 +0900

    add artificial movement for enemy (not tested)

---
+++ Defense/Assets/Script/AI/ArtificalMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ArtificalMovement : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+
+    [SerializeField]
+    private float originMoveSpeed;
+    [SerializeField]
+    private float originStopDistanceFromDest;
+
+    private float currentMoveSpeed;
+    public float MoveSpeed
+    {
+        get => currentMoveSpeed;
+        set => currentMoveSpeed = value;
+    }
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = originMoveSpeed;
+        navMeshAgent.stoppingDistance = originStopDistanceFromDest;
+        currentMoveSpeed = originMoveSpeed;
+        navMeshAgent.autoBraking = true;
+    }
+
+    public void InitSpeed()
+    {
+        currentMoveSpeed = originMoveSpeed;
+    }
+
+    public void MoveTo(Transform dest)
+    {
+        MoveTo(dest.position);
+    }
+
+    public void MoveTo(Vector3 dest)
+    {
+        navMeshAgent.velocity = Vector3.zero;
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(dest);
+    }
+}
