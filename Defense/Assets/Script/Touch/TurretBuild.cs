21049163cb7ccffc46e8d8f6f66df9584989b8d6
howudong <53307093+howudong@users.noreply.github.com>
2022-05-05 17:12:28 +0900

    씬 변경, 매쉬 오브젝트 추가 터렛 스크립트 추가

---
+++ Defense/Assets/Script/Touch/TurretBuild.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBuild : MonoBehaviour
+{
+    private GameObject target;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit,100f))
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                   if(hit.collider.GetComponent<EntityBaseInfo>() != null)
+                    {
+                    }
+                }
+
+            }
+        }
+    }
+}
