ff47a4f583d38a3282ac1e2c41ff17dda1ca3f4f
howudong <53307093+howudong@users.noreply.github.com>
2022-06-03 18:40:24 +0900

    Apply Tower Init Function in VirtualEntity Script(Test)

--- Defense/Assets/Script/VirtualEntity.cs
+++ Defense/Assets/Script/VirtualEntity.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class VirtualEntity : MonoBehaviour
 {
-    private MeshRenderer mesh;
     public bool isFixed;
     private void Start()
     {
         isFixed = false;
-        mesh = GetComponent<MeshRenderer>();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -56,22 +54,26 @@ public class VirtualEntity : MonoBehaviour
     }
     public void SetTurret()
     {
-        Color color = GetComponent<MeshRenderer>().material.color;
+        Color color = GetComponentInChildren<MeshRenderer>().material.color;
         color.a = 1f;
-        //tower.GetComponent<Tower>().enabled = true;
-        //tower.GetComponent<EntityHealth>().enabled = true;
-        //tower.GetComponent<AttackProjectile>().enabled = true;
-        GetComponent<Collider>().isTrigger = false;
-        GetComponent<MeshRenderer>().material.color = color;
+        GetComponent<Tower>().enabled = true;
+        GetComponent<EntityHealth>().enabled = true;
+        GetComponent<AttackProjectile>().enabled = true;
+        GetComponentInChildren<Collider>().isTrigger = false;
+        GetComponentInChildren<MeshRenderer>().material.color = color;
+
+        //값을 임의로 지정하여 테스트 중임
+        GetComponent<EntityHealth>().Init(100, 100);
+        GetComponent<AttackProjectile>().Init(30, 20, 1, 1);
     }
     public void SetVirtualTurret()
     {
-        Color color = GetComponent<MeshRenderer>().material.color;
+        Color color = GetComponentInChildren<MeshRenderer>().material.color;
         color.a = 0.4f;
-        //tower.GetComponent<Tower>().enabled = false;
-        //tower.GetComponent<EntityHealth>().enabled = false;
-        //tower.GetComponent<AttackProjectile>().enabled = false;
-        GetComponent<Collider>().isTrigger = true;
-        GetComponent<MeshRenderer>().material.color = color;
+        GetComponent<Tower>().enabled = false;
+        GetComponent<EntityHealth>().enabled = false;
+        GetComponent<AttackProjectile>().enabled = false;
+        GetComponentInChildren<Collider>().isTrigger = true;
+        GetComponentInChildren<MeshRenderer>().material.color = color;
     }
 }
