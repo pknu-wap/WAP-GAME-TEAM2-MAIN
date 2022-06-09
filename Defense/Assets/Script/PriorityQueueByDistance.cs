72f9cc4bc98e970024906b556eea04b9790105f1
seungmin_cho <seungmin.cho@bagelcode.com>
2022-06-09 15:28:36 +0900

    now priority queue doesn't inherit mono

--- Defense/Assets/Script/PriorityQueueByDistance.cs
+++ Defense/Assets/Script/PriorityQueueByDistance.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PriorityQueueByDistance : MonoBehaviour
+public class PriorityQueueByDistance
 {
     struct LocationDatas
     {
